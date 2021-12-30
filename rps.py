f7f77f97a2759e1d8edc5c8321da451dda7247ba
shayan15sa <90975003+shayan15sa@users.noreply.github.com>
2021-12-30 14:46:41 +0330

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,6 +1,9 @@
 import emoji
+import random
 
 print("Hi Let's play \n" + "paper or sicsor or rock")
 inp = input()
 if inp == "p" or inp == "r" or inp == "s":
     exit()
+list = ["p", "r", "s"]
+ans = random.choice(list)
