9d41122d4594c24dc4d96edade4486346762d391
shayan15sa <90975003+shayan15sa@users.noreply.github.com>
2021-12-30 14:52:32 +0330

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,9 +1,16 @@
 import emoji
 import random
 
-print("Hi Let's play \n" + "paper or sicsor or rock")
+print("Hi Let's play \n" + "paper or scissor or rock")
 inp = input()
 if inp == "p" or inp == "r" or inp == "s":
     exit()
 list = ["p", "r", "s"]
 ans = random.choice(list)
+
+if ans == "p":
+    print(emoji.emojize(":memo:"))
+elif ans == "s":
+    print(emoji.emojize(":scissor:"))
+elif ans == "r":
+    print(emoji.emojize(":mountain:"))
