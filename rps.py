1bf5d38d5ac01911c3cd5599c3c2bad54b648735
shayan15sa <90975003+shayan15sa@users.noreply.github.com>
2021-12-30 15:31:01 +0330

    beta

--- rps.py
+++ rps.py
@@ -2,10 +2,18 @@ import emoji
 import random
 import sys
 import time
+from os import system, name
 
+
+def clear():
+    if name == "nt":
+        _ = system("cls")
+
+
+clear()
 print("Hi Let's play \n" + "paper or scissor or rock")
 inp = input()
-if inp == "p" or inp == "r" or inp == "s":
+if inp == "p" and inp == "r" and inp == "s":
     exit()
 list = ["p", "r", "s"]
 ans = random.choice(list)
@@ -23,9 +31,35 @@ def animate():
     sys.stdout.write("\rDone!     ")
 
 
+animate()
+
+
+clear()
+if inp == "p":
+    print("you : " + emoji.emojize(":memo:"))
+elif inp == "s":
+    print("you : " + emoji.emojize(":scissors:"))
+elif inp == "r":
+    print("you : " + emoji.emojize(":mountain:"))
+
 if ans == "p":
-    print(emoji.emojize(":memo:"))
+    print("pc : " + emoji.emojize(":memo:"))
 elif ans == "s":
-    print(emoji.emojize(":scissor:"))
+    print("pc : " + emoji.emojize(":scissors:"))
 elif ans == "r":
-    print(emoji.emojize(":mountain:"))
+    print("pc : " + emoji.emojize(":mountain:"))
+
+
+if inp == ans:
+    print("Draw!!")
+    exit()
+if inp == "p" and ans == "r":
+    print("WIN!!")
+    exit()
+if inp == "r" and ans == "s":
+    print("WIN!!")
+    exit()
+if inp == "s" and ans == "p":
+    print("WIN!!")
+    exit()
+print("LOSE!!")
